584730d82ff0ddbb0b1d7045fb307fc4b97362a6
Alany Lourenço <95310758+AlanyLourenco@users.noreply.github.com>
2023-10-11 21:10:20 -0300

    Update CelulaTest.java

--- Atividade1110/src/test/java/com/example/CelulaTest.java
+++ Atividade1110/src/test/java/com/example/CelulaTest.java
@@ -6,7 +6,21 @@ import static org.junit.Assert.assertEquals;
 public class CelulaTest {
 
     @Test
-    public void testAtualizarValorENotificacao() {
+    public void testNotificacaoSimples() {
+        Planilha planilha = new Planilha();
+        Celula a1 = planilha.criarCelula("A1", 10);
+        Celula a2 = planilha.criarCelula("A2", 20);
+        Celula a3 = planilha.criarCelula("A3", 0);
+
+        a3.adicionarObservadora(a1);
+
+        a1.atualizarValor(15);
+
+        assertEquals(25, a3.getValor()); // A3 deve ser notificada e atualizada para 25
+    }
+
+    @Test
+    public void testNotificacaoDupla() {
         Planilha planilha = new Planilha();
         Celula a1 = planilha.criarCelula("A1", 10);
         Celula a2 = planilha.criarCelula("A2", 20);
@@ -17,6 +31,21 @@ public class CelulaTest {
 
         a1.atualizarValor(15);
 
-        assertEquals(45, a3.getValor()); // O novo valor de A3 deve ser 45
+        assertEquals(45, a3.getValor()); // A3 deve ser notificada e atualizada para 45
+    }
+
+    @Test
+    public void testNotificacaoCircular() {
+        Planilha planilha = new Planilha();
+        Celula a1 = planilha.criarCelula("A1", 10);
+        Celula a2 = planilha.criarCelula("A2", 20);
+        Celula a3 = planilha.criarCelula("A3", 0);
+
+        a1.adicionarObservadora(a2);
+        a2.adicionarObservadora(a1);
+
+        a1.atualizarValor(15);
+
+        assertEquals(25, a3.getValor()); // A3 deve ser notificada e atualizada para 25
     }
 }
